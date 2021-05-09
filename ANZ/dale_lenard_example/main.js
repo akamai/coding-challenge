0c73816251f7b51d65761d5a1181fae24300db87
Dale Lenard <dale_lenard@hotmail.com>
2021-05-10 08:35:38 +1000

    Create main.js

---
+++ ANZ/dale_lenard_example/main.js
@@ -0,0 +1,14 @@
+export function onClientRequest(request) {
+
+}
+
+export function onOriginRequest(request) {
+}
+
+export function onOriginResponse(request, response) {
+
+}
+
+export function onClientResponse(request, response) {
+
+}
